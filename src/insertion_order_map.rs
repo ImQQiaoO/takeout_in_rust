08247e019ad06c16abcf4b56f8be081c8f814215
ImQQiaoO <1727344097@qq.com>
2025-03-21 15:56:06 +0800

    pref: 优化保证插入顺序字典的时间复杂度

--- src/insertion_order_map.rs
+++ src/insertion_order_map.rs
@@ -1,26 +1,32 @@
 use rand::seq::SliceRandom;
+use std::collections::HashMap;
 
 #[derive(Default)]
 pub struct InsertionOrderMap<K, V> {
     data: Vec<(K, V)>,
+    indices: HashMap<K, usize>,
 }
 
-impl<K: Eq + Ord, V> InsertionOrderMap<K, V> {
+impl<K: Eq + Ord + Clone + std::hash::Hash, V> InsertionOrderMap<K, V> {
     pub fn new() -> Self {
-        Self { data: Vec::new() }
+        Self {
+            data: Vec::new(),
+            indices: HashMap::new(),
+        }
     }
-
-    // TODO: fix target: time complexity in O(n)
+    
     pub fn insert(&mut self, key: K, value: V) {
-        if let Some(pos) = self.data.iter().position(|(k, _)| k == &key) {
-            self.data[pos] = (key, value);
+        if let Some(&pos) = self.indices.get(&key) {
+            self.data[pos].1 = value;
         } else {
-            self.data.push((key, value));
+            let pos = self.data.len();
+            self.data.push((key.clone(), value));
+            self.indices.insert(key, pos);
         }
     }
 
     pub fn get(&self, key: &K) -> Option<&V> {
-        self.data.iter().find(|(k, _)| k == key).map(|(_, v)| v)
+        self.indices.get(key).map(|&pos| &self.data[pos].1)
     }
 
     pub fn iter(&self) -> impl Iterator<Item = (&K, &V)> {
@@ -43,7 +49,7 @@ impl<K: Eq + Ord, V> InsertionOrderMap<K, V> {
     where
         F: FnOnce(&mut V),
     {
-        if let Some(pos) = self.data.iter().position(|(k, _)| k == key) {
+        if let Some(&pos) = self.indices.get(key) {
             update_fn(&mut self.data[pos].1);
         }
     }
@@ -51,9 +57,17 @@ impl<K: Eq + Ord, V> InsertionOrderMap<K, V> {
     pub fn shuffle(&mut self) {
         let mut rng = rand::thread_rng();
         self.data.shuffle(&mut rng);
+        
+        for (i, (k, _)) in self.data.iter().enumerate() {
+            self.indices.insert(k.clone(), i);
+        }
     }
 
     pub fn sort_by_key(&mut self) {
         self.data.sort_by(|a, b| a.0.cmp(&b.0));
+        
+        for (i, (k, _)) in self.data.iter().enumerate() {
+            self.indices.insert(k.clone(), i);
+        }
     }
 }
